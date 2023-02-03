b388206b49cd21b37f6e90c28b21b6a4cbb44847
Tarun Sharma <sharmas771@gmail.com>
2023-02-03 12:18:12 +0530

    add two level(form display and action level ) security/authentication that user cant post without login

--- routes/posts.js
+++ routes/posts.js
@@ -3,6 +3,6 @@ const passport = require('passport');
 const router  = express.Router();
 const postController = require('../controllers/post_controller');
 
-router.post('/create',postController.create);
+router.post('/create',passport.checkAuthentication,postController.create);
 
 module.exports=router;
