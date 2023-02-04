46a4dd99ab17413a0f56004ab1543f3d88cf81f7
Tarun Sharma <sharmas771@gmail.com>
2023-02-04 15:01:03 +0530

    creating delete post functionality

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -10,3 +10,19 @@ module.exports.create = (req,res)=>{
         return res.redirect('back');
     });
 }
+
+module.exports.destroy = (req,res)=>{
+    Post.findById(req.params.id,(err,post)=>{
+        if(err){console.log('Error in finding post during deleting',err); return}
+        // .id means converting the object id into string otherwise we use ._id
+        if(post.user==req.user.id){
+            post.remove();
+            Comment.deleteMany({post:req.params.id},(err)=>{
+                if(err){console.log('Error in deleting comments while deleting post',err); return }
+                return res.redirect('back');
+            })
+        }else{
+            return res.redirect('back');
+        }
+    })
+}
