d21071a850f5717c3eb02f03a9eb2268a4189c37
Tarun Sharma <115355429+fsdsharmas771@users.noreply.github.com>
2023-04-11 18:19:01 +0530

    Update nodemailer.js

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -7,8 +7,8 @@ let transporter = nodemailer.createTransport({
     port: 587,
     secure: false, // true for 465, false for other ports
     auth: {
-      user: 'codeialteam4@gmail.com', // generated ethereal user
-      pass: 'blthdzdbjskjublq', // generated ethereal password
+      user: 'xxxxxxxx@gmail.com', // generated ethereal user
+      pass: 'xxxxxxxxxxxxxx', // generated ethereal password
     },
 });
 
